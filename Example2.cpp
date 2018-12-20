135745f8a31bee1d5164659d11bafd3845ad32ab
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-20 13:42:02 +0800

    Update Example2.cpp

--- Example2.cpp
+++ Example2.cpp
@@ -1,11 +1,32 @@
-/* C++ Program to return absolute value of variable types
-integer and float using function overloading */
+/**
+* @file Example2.cpp
+*
+* @brief C++ Program to return absolute value of variable types
+* integer and float using function overloading.
+*
+* @author Saif Ullah Ijaz
+*
+*/
 
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
+// FUNCTION PROTOTYPE (DECLARATION)
+
+/** function that returns absoulte of an integer.
+*
+* @param var The integer number to find absolute.
+*
+* @return absolute value of the integer input.
+*/
 int absolute(int);
+
+/**
+# @overload float absolute(float);
+*/
 float absolute(float);
+
+// function main begins program execution
 int main() {
 	int a = -5;
 	float b = 5.5;
@@ -15,15 +36,22 @@ int main() {
 	system("pause");
 	return 0;
 }
+// end main 
 
+// FUNCTION DEFINITION
+
+// takes integer as input
 int absolute(int var) {
 	if (var < 0)
 		var = -var;
 	return var;
 }
+// end function absolute
 
+// takes float as input
 float absolute(float var) {
 	if (var < 0.0)
 		var = -var;
 	return var;
 }
+// end function absolute
